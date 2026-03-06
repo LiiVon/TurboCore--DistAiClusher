89e33a4d771ea7c55f115e8be73aed65ce066919
ZeLi <2671749518@qq.com>
2026-03-06 22:05:36 +0800

    Update TcpClient.cpp

--- Net/TcpClient.cpp
+++ Net/TcpClient.cpp
@@ -2,7 +2,6 @@
 #include "logger.h"
 #include "socket_utils.h"
 
-
 namespace TcFrame
 {
 	TcpClient::TcpClient(EventLoop* loop, const Address& server_addr, std::string name)
@@ -40,7 +39,6 @@ namespace TcFrame
 		m_socket = std::make_unique<Socket>(SocketUtils::CreateNonBlockingSocket());
 		m_socket->SetNonBlocking(true);
 
-		
 		int ret = m_socket->Connect(m_server_addr);
 		if (ret == 0)
 		{
@@ -50,37 +48,37 @@ namespace TcFrame
 		}
 		else if (ret == SocketUtils::kConnectInProgress)
 		{
-			// 等待连接完成，把socket加入EventLoop监听写事件
+			// 连接正在进行，等待连接完成，把socket加入EventLoop监听写事件
 			LOG_DEBUG("TcpClient " + m_name + " connect in progress, wait for completion");
-
 			std::unique_ptr<Channel> temp_channel = std::make_unique<Channel>(m_loop, m_socket->GetFd());
-			temp_channel->SetWriteCallback([this, temp = std::move(temp_channel)]() mutable {
-				// 写事件触发，连接完成，检查错误
-				SocketType fd = temp->GetFd();
-				int err = SocketUtils::GetSocketError(fd);
-				if (err != 0)
+			Channel* temp_ptr = temp_channel.get(); // 拿裸指针给lambda捕获，满足std::function可拷贝要求
+			temp_channel->SetWriteCallback([this, temp_ptr]()
 				{
-					LOG_ERROR("TcpClient " + m_name + " connect failed, error: " + SocketUtils::GetLastErrorStr(err));
-					m_connecting = false;
-					// 👉 连接失败，自动重连开启就重试
-					if (m_auto_reconnect && m_started)
+					// 写事件触发，连接完成，检查错误
+					SocketType fd = temp_ptr->GetFd();
+					int err = SocketUtils::GetSocketError(fd);
+					if (err != 0)
 					{
-						LOG_INFO("TcpClient " + m_name + " will retry connect after " + std::to_string(m_reconnect_delay_ms) + "ms");
-						m_loop->RunAfter(m_reconnect_delay_ms / 1000.0, [this]() { DoReconnect(); });
+						LOG_ERROR("TcpClient " + m_name + " connect failed, error: " + SocketUtils::GetLastErrorStr(err));
+						m_connecting = false;
+						if (m_auto_reconnect && m_started)
+						{
+							LOG_INFO("TcpClient " + m_name + " will retry connect after " + std::to_string(m_reconnect_delay_ms) + "ms");
+							m_loop->RunAfter(m_reconnect_delay_ms / 1000.0, [this]() { DoReconnect(); });
+						}
 					}
-				}
-				else
-				{
-					// 连接成功，处理
-					HandleConnect(std::move(*m_socket));
-					m_socket.reset();
-				}
-				
-				if (temp->IsAdded())
-				{
-					m_loop->RemoveChannel(temp.get());
-				}
-				// temp离开作用域自动析构，RAII，和你设计一致
+					else
+					{
+						// 连接成功，处理
+						HandleConnect(std::move(*m_socket));
+						m_socket.reset();
+					}
+					// 临时Channel用完就移除
+					if (temp_ptr->IsAdded())
+					{
+						m_loop->RemoveChannel(temp_ptr);
+					}
+					// temp_channel离开作用域自动析构，RAII保证释放，没有内存泄漏
 				});
 			temp_channel->EnableWriting();
 			m_loop->UpdateChannel(temp_channel.get());
@@ -101,21 +99,12 @@ namespace TcFrame
 		m_connecting = false;
 		LOG_INFO("TcpClient " + m_name + " connected to " + m_server_addr.ToString() + " fd: " + std::to_string(static_cast<int>(client_socket.GetFd())));
 
-	
 		TcpConnectionPtr conn = std::make_shared<TcpConnection>(m_loop, m_name, std::move(client_socket), m_server_addr);
 
-		
 		conn->SetConnectionCallback(m_connection_callback);
 		conn->SetMessageCallback(m_message_callback);
 		conn->SetWriteCompleteCallback(m_write_complete_callback);
 		conn->SetCloseCallback([this](const TcpConnectionPtr& conn) { HandleRemoveConnection(conn); });
-	
-		if (m_close_callback)
-		{
-			// 先调用我们的移除处理，再调用用户的，顺序正确
-			// 哦，不对，我们已经在HandleRemoveConnection里处理了，这里把用户回调存在我们这里，所以改成：
-			// 我们已经保存了用户的m_close_callback，在HandleRemoveConnection最后调用
-		}
 
 		m_connection = conn;
 		conn->ConnectEstablished();
@@ -126,14 +115,12 @@ namespace TcFrame
 		m_loop->AssertInLoopThread();
 		LOG_INFO("TcpClient " + m_name + " connection disconnected from " + m_server_addr.ToString());
 
-	
 		m_connection.reset();
-	
+
 		if (m_close_callback)
 		{
 			m_close_callback(conn);
 		}
-
 
 		if (m_auto_reconnect && m_started)
 		{
@@ -167,7 +154,6 @@ namespace TcFrame
 		LOG_INFO("TcpClient " + m_name + " disconnected from " + m_server_addr.ToString());
 	}
 
-	
 	void TcpClient::Send(const std::string& message)
 	{
 		Send(message.data(), message.size());
